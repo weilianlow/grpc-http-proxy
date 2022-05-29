7bc22247f411e5e002e0fe1ba8a2b5e9cbfb6f9a
weilianlow <weilian.low88@gmail.com>
2022-05-29 16:52:48 +0800

    remove files

--- download_proto.sh
+++
@@ -1,5 +0,0 @@
-source config.sh
-rm -rf "$PROTO_BASE_FOLDER"
-mkdir "$PROTO_BASE_FOLDER"
-cd "$PROTO_BASE_FOLDER"
-git clone -b "$BRANCH" "$GIT_REPO_PROTO" || exit 1
--- server.sh
+++
@@ -1,2 +0,0 @@
-source config.sh
-python sample_server.py
